2b3f3bc34ecf694ced8b8f0dcefd8adaf63fefcc
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2024-01-30 10:35:17 +0500

    getUsers route added

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUsers() {
+    return [
+      {
+        name: 'ilyosbek',
+        age: 20,
+      },
+    ];
   }
 }
