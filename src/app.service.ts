366b8ff0286a06410b3c1f63dca5f4fad9fdf845
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2024-01-30 11:02:36 +0500

    prisma added

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,13 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-
-  getUsers() {
-    return [
-      {
-        name: 'ilyosbek',
-        age: 20,
-      },
-    ];
-  }
 }
